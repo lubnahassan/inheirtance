082d10785f22123b4411012eb4c26359c8064a19
lubnahassan <36442977+lubnahassan@users.noreply.github.com>
2018-11-17 10:58:37 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,115 +1,41 @@
 #include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-class pet
+
+void rev(char arr[],int lower,int upper)
 {
-protected:
-    string name;
-public:
-    pet (string name )
-    {
-        this->name=name;
+    if(lower<upper){
+        swap(arr[lower],arr[upper]);
+        rev(arr,lower+1,upper-1);
     }
-    void Setname(string n)
-    {
-         name=n;
-    }
-    virtual void printDescription() = 0;
-};
-class cat : public pet
+}
+
+void rev(char arr[])
 {
-private:
-    bool neuterSpayed;
-public:
-   //cat ()
-    //{
-      //   neuterSpayed=0;
-    //}
-   cat ( string name ,bool n ) : pet (name)
-    {
-         neuterSpayed=n;
-    }
-    void Setspayed(bool x)
-    {
-        neuterSpayed=x;
-    }
-    bool Getspayed ()
-    {
-      return  neuterSpayed;
-    }
-    void printDescription()
-    {
-           cout << "Cat named " << name << endl;
-           cout << "Neuter/Spayed: " << neuterSpayed << endl << endl;
-    }
-
-};
-class dog : public pet
-{
-private:
-     bool neuterSpayed;
-public:
-    //dog ()
-    //{
-    //neuterSpayed = 0;
-    //}
-    dog ( string name ,bool s) : pet (name)
-    {
-        neuterSpayed= s;
-    }
-     void Setspayed(bool x)
-    {
-        neuterSpayed=x;
-    }
-    bool Getspayed ()
-    {
-      return neuterSpayed;
-    }
-
-     void printDescription()
-    {
-           cout << "Dog named " << name << endl;
-           cout << "Neuter/Spayed: " <<neuterSpayed << endl << endl;
-
-    }
-};
-
-class bird : public pet
-{
-private:
-    bool talks;
-public:
- bird ( string name ,bool s) : pet (name)
-    {
-        talks=s;
-    }
-     void Settalk(bool x)
-    {
-        talks=x;
-    }
-    bool Getspayed ()
-    {
-      return talks;
-    }
-    void printDescription( )
-    {
-           cout << "Bird named " << name << endl;
-        cout << "Talks: " << talks << endl << endl;
-    }
-
-};
-
+    int upper=strlen(arr),lower=0;
+    rev(arr,lower,upper);
+}
 
 int main()
 {
-  pet *p[3];
-  cat c("bosy" , 1);
-  dog d("jonny " , 0);
-  bird b("xxx" , 1);
-  p[0] = &c ;
-   p[1]= &d ;
-  p[2]=&b;
-  for(int i = 0;i < 3;i++)
-    p[i] -> printDescription();
-    return 0;
+    int upper,lower;
+    char x[]="Ahmed";
+    int y=strlen(x);
+    rev(x,1,4);
+    for(int i=0;i<y;++i){
+        cout<<x[i];
+    }
+     cout<<endl;
+     a:
+    cin >>lower>>upper;
+    if (upper>y)
+    {
+        cout<<"this size is very big !!"<<endl;
+        goto a;
+    }
+    cout<<endl;
+    rev(x);
+    for(int i=1;i<y+1;++i){
+        cout<<x[i];
+    }
 }
